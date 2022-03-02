019dd26a4f9001c2bb7dc7e0ba1693c8eb5d1557
avongalie <alexsiaavila@gmail.com>
2022-03-02 15:03:30 -0500

    created intervals

--- scripts.js
+++ scripts.js
@@ -17,6 +17,7 @@ function startGame(){
     let name = input.value;
     let newPet = createPet(name);
     console.log(newPet)
+    startIntervals();
 }
 
 
@@ -28,7 +29,18 @@ function createPet(name){
 
 //sets timer for boredom hunger and sleepiness;
 function startIntervals(){
-
+    var bored = setInterval(function(){
+            boredom++;
+            console.log(`boredem: ${boredom}`);
+        },4000)
+    var hungry = setInterval(function(){
+            hunger++;
+            console.log(`hunger: ${hunger}`);
+        },6000)
+    var sleep = setInterval(function(){
+        sleepiness++;
+            console.log(`sleepiness: ${sleepiness}`);
+        },10000)
 }
 
 //starts game when button is clicked
